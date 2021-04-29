93bdfb845f2fe773002eff564ecad483e03698a5
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-04-29 08:52:47 +0200

    Created a fuction that uses a reular expression to validate the email input

--- assets/js/form.js
+++ assets/js/form.js
@@ -20,3 +20,11 @@ const fields = [
     "#password",
     "#confirmPassword",
 ];
+
+let isFormValid = false;
+isValidationOn = false;
+
+const isValidEmail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
